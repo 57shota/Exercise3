18011f137cc94006b729f39d16801fd91ff8dffe
Shota Ito <sho.weblife@gmail.com>
2019-05-19 17:16:02 +1000

    created packages

--- src/Utilities/InputOutput.java
+++ src/Utilities/InputOutput.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exercise3;
+package Utilities;
 
 /**
  *
  * @author sho
  */
-public class MeetingCalendar {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class InputOutput {
     
 }
