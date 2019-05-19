31eec2ca14932b6443576b033905352a799706d6
Shota Ito <sho.weblife@gmail.com>
2019-05-20 00:36:54 +1000

    created read/saveAppodta

--- src/Utilities/InputOutput.java
+++ src/Utilities/InputOutput.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Utilities;
+
+import Classes.Appointment;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
  * @author sho
  */
 public class InputOutput {
-    
+    public static ArrayList<Appointment> readAppointmentData(String file) throws FileNotFoundException, IOException{
+        
+        ArrayList<Appointment> list = new ArrayList<>();
+        FileInputStream fin = new FileInputStream(file);
+        DataInputStream din = new DataInputStream(fin);
+        Appointment appo;
+        
+        while(din.available() > 0) { 
+            appo = new Appointment(din.readInt(), din.readInt(), din.readUTF());
+            list.add(appo);
+        }
+        return list;
+    }
+
+    public static void saveAppointmentData(String file, ArrayList<Appointment> list) throws FileNotFoundException, IOException{
+        
+        FileOutputStream fos = new FileOutputStream(file);
+        DataOutputStream dos = new DataOutputStream(fos);
+        
+        for(Appointment appo: list){
+            dos.writeInt(appo.getDay());
+            dos.writeInt(appo.getHour());
+            dos.writeUTF(appo.getName());
+        }
+        dos.close();
+    }
 }
